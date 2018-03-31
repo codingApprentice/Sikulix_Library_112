6a4868194fba46123bc33e1d796a6c95659f446f
Barry O'Farrell <barry7ie@gmail.com>
2018-03-31 18:43:02 +0100

    change imports

--- io.py
+++ io.py
@@ -77,7 +77,7 @@ import _jyio
 
 
 
-from _jyio import BufferedRandom
+
 from _jyio import IncrementalNewlineDecoder
 from _jyio import TextIOWrapper
 			 
@@ -90,6 +90,7 @@ from _jyio import StringIO
 from _jyio import BufferedReader
 from _jyio import BufferedWriter
 from _jyio import BufferedRWPair
+from _jyio import BufferedRandom
 
 OpenWrapper = _jyio.open # for compatibility with _pyio
 
